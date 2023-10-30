abbc6f70bef275c007928899862b22864221afdf
Z. Chris <chriszoelln@gmail.com>
2023-10-30 11:51:22 +0100

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,25 @@
 "use client";
 import { Icon, Divider } from "@tremor/react";
-import { HomeIcon } from "@heroicons/react/solid";
+import { HomeIcon, UserIcon, CurrencyDollarIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className="w-min bg-slate-700 rounded-lg">
-      <ul className="flex flex-col justify-center items-center p-2 space-y-3">
-        <li>
-          <Icon icon={HomeIcon} size="lg" color="indigo" />
-        </li>
+    <div className="w-min h-screen bg-slate-800 rounded-lg hidden md:block">
+      <ul className="flex flex-col justify-center items-center p-4 space-y-3">
+        <Link href="/" className="bg-slate-700 rounded-lg my-5">
+          <Icon icon={HomeIcon} size="lg" />
+        </Link>
 
         <Divider />
-        <li>
-          <Icon icon={HomeIcon} size="lg" color="indigo" />
-        </li>
+
+        <Link href="/customers" className="bg-slate-700 rounded-lg my-5">
+          <Icon icon={UserIcon} size="lg" />
+        </Link>
+
+        <Link href="/orders" className="bg-slate-700 rounded-lg my-5">
+          <Icon icon={CurrencyDollarIcon} size="lg" />
+        </Link>
       </ul>
     </div>
   );
